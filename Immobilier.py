b07db3b86b26a3921f25ed8af858a8e74e33ff75
Nuggets54 <111306453+Nuggets54@users.noreply.github.com>
2023-09-06 14:05:27 +0200

    Working script, linear regression with R²

--- Immobilier.py
+++ Immobilier.py
@@ -8,27 +8,28 @@ import gzip
 import urllib.request
 import matplotlib.pyplot as plt
 from sklearn import linear_model
-import csv
 from sklearn import model_selection
-from sklearn import neighbors
-from sklearn import neighbors, metrics
 from sklearn.metrics import mean_squared_error, r2_score
 
 dirname = os.path.dirname(os.path.abspath(__file__))
-#department = "34"
+DEPARTEMENT = "34"
+CITY = "Montpellier"
+TYPE = "Maison"
+MUTATION = "Vente"
+MAX_PRICE = 1500000
+MAX_SURFACE = 250
 
-def download(annee, department = "34"):
+def download(annee, department = DEPARTEMENT):
     """
     Download rents data from https://files.data.gouv.fr concerning specified french department
     """
     CSV_URL = "https://files.data.gouv.fr/geo-dvf/latest/csv/" + annee + "/departements/"+ department + ".csv.gz"
-    filename = dirname + '/data/34_' + annee +'.csv'
-    if not exists(dirname + '/data/34_' + annee + '.csv'):
+    filename = dirname + '/data/'+ department +'_' + annee +'.csv'
+    if not exists(dirname + '/data/'+ department +'_' + annee + '.csv'):
         with urllib.request.urlopen(CSV_URL) as response:
              with gzip.GzipFile(fileobj=response) as uncompressed:
                 file_content = uncompressed.read()
         with open(filename, 'wb') as f:
-            #file_content = file_content.replace(bytes(',', 'utf8'),bytes(';', 'utf8'))
             f.write(file_content)
     #if data not available -> pass 
 
@@ -40,7 +41,7 @@ for i in range(5):
     date = currentDateTime.date()
     year = int(date.strftime("%Y"))
     year -= i + 1
-    years[str(year)] = dirname + '\\data\\34_' + str(year) + '.csv'
+    years[str(year)] = dirname + '\\data\\'+ CITY +'_' + str(year) + '.csv'
 
 for year in years:
     download(year)
@@ -54,100 +55,66 @@ df = pd.concat((pd.read_csv(f, sep=",", decimal='.',
                              for f in all_files), ignore_index=True)
 
 # Filtering
-df = df[(df.type_local == 'Maison') & (df.nom_commune == 'Montpellier') &
-         (df.nature_mutation == 'Vente') & (df.valeur_fonciere == df.valeur_fonciere) &
-          (df.valeur_fonciere < 10000000)]
+df = df[(df.type_local == TYPE) & (df.nom_commune == CITY) &
+         (df.nature_mutation == MUTATION) &
+          (df.valeur_fonciere < MAX_PRICE) & (df.surface_reelle_bati < MAX_SURFACE)]
 
-# # Reshape data to include gardens (data aggregation)
-#df['key']=df.groupby(['id_mutation','valeur_fonciere']).cumcount()
-
-# df = df.pivot(columns=['nature_culture'], values = ['date_mutation', 'nature_mutation',
-#         'valeur_fonciere', 'code_commune', 'nom_commune', 'surface_reelle_bati',
-#        'nombre_pieces_principales', 'type_local', 'surface_terrain'])
-
-# Remove duplicates based on id
+# Remove duplicates basing on id
 df = df.drop_duplicates(subset=['id_mutation'], keep='first')
+df = df.dropna()
 df = df.reset_index(drop = True)
-x = df['surface_reelle_bati'].values
-X = df.loc[:,['date_mutation','nom_commune','nombre_pieces_principales','surface_terrain','surface_reelle_bati']].values
+X = df[['nombre_pieces_principales','surface_terrain','surface_reelle_bati']]
+y = df[['valeur_fonciere']]
 
-y = df['valeur_fonciere'].values
-x = x.reshape(-1, 1)
-y = y.reshape(-1, 1)
-
-# Séparation test/entrainement
+# Spliting test/training dataset
 X_train, X_test, y_train, y_test = \
-	model_selection.train_test_split(X, y,
+	model_selection.train_test_split(X,
+                                    y,
                                 	test_size=0.3 # 30% des données dans le jeu de test
                                 	)
 
-# Visualisation
-# Prix/surface
-plt.scatter(X[:,4], y, color='coral')
+# Linear regression
+lr = linear_model.LinearRegression()
+lr_baseline = lr.fit(X_train[['surface_reelle_bati']], y_train)
+baseline_pred = lr_baseline.predict(X_test[['surface_reelle_bati']])
+
+def sumsq(x,y):
+    return sum((x - y)**2)
+
+def r2score(pred, target):
+    return 1 - sumsq(pred, target) / sumsq(target, np.mean(target))
+
+score_bl = r2score(baseline_pred[:,0], y_test['valeur_fonciere'])
+
+plt.plot(X_test[["surface_reelle_bati"]], y_test, 'ro', markersize = 5)
+plt.plot(X_test[["surface_reelle_bati"]], baseline_pred, color="coral", linewidth = 2)
+plt.title('R²: ' + str(round(score_bl,2)))
 plt.show()
-# Régression linéaire
-regr = linear_model.LinearRegression()
-regr.fit(x, y)
+print(score_bl)
 
+# # Running Evaluation Metrics
+# predictions = lr.predict(X_test[['surface_reelle_bati']])
+# r2 = r2_score(predictions, y_test['valeur_fonciere'])
+# rmse = mean_squared_error(y_test, predictions, squared=False)
+# print('The r2 is: ', r2)
+# print('The rmse is: ', rmse)
 
-# Running Evaluation Metrics
-predictions = regr.predict(X_test)
-r2 = r2_score(y_test, predictions)
-rmse = mean_squared_error(y_test, predictions, squared=False)
-print('The r2 is: ', r2)
-print('The rmse is: ', rmse)
+# plt.scatter(predictions, y_test, color='coral')
+# plt.show()
+# # The coefficients
+# print("Coefficients: \n", lr.coef_)
+# # The mean squared error
+# print("Mean squared error: %.2f" % mean_squared_error(y_test, y_pred))
+# # The coefficient of determination: 1 is perfect prediction
+# print("Coefficient of determination: %.2f" % r2_score(y_test, y_pred))
 
-plt.scatter(y, predictions, color='coral')
-plt.show()
-# The coefficients
-print("Coefficients: \n", regr.coef_)
-# The mean squared error
-print("Mean squared error: %.2f" % mean_squared_error(y_test, y_pred))
-# The coefficient of determination: 1 is perfect prediction
-print("Coefficient of determination: %.2f" % r2_score(y_test, y_pred))
+# # Plot outputs
+# plt.scatter(X_test, y_test, color="black")
+# plt.plot(X_test, y_pred, color="blue", linewidth=3)
 
-# Plot outputs
-plt.scatter(X_test, y_test, color="black")
-plt.plot(X_test, y_pred, color="blue", linewidth=3)
+# plt.xticks(())
+# plt.yticks(())
 
-plt.xticks(())
-plt.yticks(())
-
-plt.show()
-
-regr.predict(100)
-
-
-
-
-# #Entraînement d'un kNN avec k = 11
-# knn = neighbors.KNeighborsRegressor(n_neighbors=11)
-
-# knn.fit(x_train, y_train)
-# A = knn.kneighbors_graph(x)
-
-# #Application pour prédire jeu de test
-# y_pred = knn.predict(x_test)
-
-# #RMSE associée
-# print("RMSE : {:.2f}".format(np.sqrt( metrics.mean_squared_error(y_test, y_pred) )))
-
-# plt.scatter(y_test, y_pred, color='coral')
 # plt.show()
 
-# y_pred_random = np.random.randint(np.min(y), np.max(y), y_test.shape)
-# #RMSE bien plus élevé qu'avec le modèle, qui a donc bien réussi à apprendre
-# print("RMSE : {:.2f}".format(np.sqrt(metrics.mean_squared_error(y_test, y_pred_random))))
-# #Write dataframe to csv
-
-# print(knn.predict(100))
-
 df.to_csv('34.csv')
-
-#Ajouter un ratio prix/surface permettant de réaliser une classification (donné/peu cher/cher/un peu cher/très cher)
-#Distinguer bâti/jardins -> surface terrain/bâti
-#+ Classes d'éloignement
-
-#Modélisation
-
-#Ajouter la distance à Mtp/une agglo ? -> Si Mtp seul, villes < 50 km ?
